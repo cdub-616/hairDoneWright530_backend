d73e46b2d4630cfb5ddeb88ac9db82064917e4c5
cdub-616 <cdub616@aol.com>
2024-04-27 16:01:14 -0700

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -982,7 +982,7 @@ async function queryNewUserFromUserID(userId) {
 
 app.get('/queryNewUserFromUserID', async (req, res) => {
     try {
-        const userId = req.query.email;
+        const userId = req.query.userId;
         if (!userId) {
             throw new Error('Invalid request. Missing "userId"');
         }
@@ -1054,7 +1054,7 @@ app.get('/allPastAppointmentsQuery', async (req, res) => {
 async function allPastAppointmentsQuery(todaysDate){
     try {
         const poolConnection = await connect();
-        const query = `SELECT Appointments.UserID, FirstName, LastName, AppointmentDate, TypeOfAppointment, AppointmentNotes
+        const query = `SELECT Appointments.UserID, FirstName, LastName, AppointmentDate, TypeOfAppointment, AppointmentNotes 
             FROM Appointments JOIN Clients ON Appointments.UserID = Clients.UserID 
             WHERE AppointmentDate < '${todaysDate}'`;
         const resultSet = await poolConnection
@@ -1239,8 +1239,8 @@ app.put('/updateClientApproval', async (req, res) =>{
         {
             throw new Error("Invalid request. Missing 'UserID'")
         }
-        const result = await UpdateClientApproval(userID);
-        res.send(result);
+        await UpdateClientApproval(userID);
+        res.send("Completed");
     }
     catch
     {
@@ -1255,9 +1255,8 @@ async function UpdateClientApproval(userID)
     {
         const poolConnection = await connect();
         const query = "UPDATE NewClients SET ApprovalStatus = 0 WHERE UserID = " + userID + ";"
-        const resultSet = await poolConnection.request().query(query);
+        await poolConnection.request().query(query);
         poolConnection.close();
-        return sortingResults(resultSet);
     }
     catch(err)
     {
@@ -1494,3 +1493,25 @@ app.get('/queryAllAppointmentsByUserID', (req, res) =>{
         res.status(500).send('Internal Server Error');
     })
 });
+
+app.delete('/deleteNewClientsByUserID', (req, res) =>{
+    const userID = req.query.userID;
+    const query = `DELETE FROM NewClients WHERE UserID = ${userID}`;
+    customQueryNoReturn(query)
+    .then((ret) => res.send(ret))
+    .catch(err => {
+        console.error('Error deleting a new client:', err.message);
+        res.status(500).send('Internal Server Error');
+    })
+});
+
+async function customQueryNoReturn(queryString){
+    try {
+        const poolConnection = await connect();
+        await poolConnection.request().query(queryString);
+        poolConnection.close();
+    } catch (err) {
+        console.error(err.message);
+        throw err;
+    }
+}
